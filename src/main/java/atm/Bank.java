39a9d556ce08615ae5971605c67f93fb54bf7116
Chidchaya <chidchaya.j@ku.th>
2022-07-19 21:33:44 +0700

    Lab4 SE Spring-IoC Annotation config

--- src/main/java/atm/Bank.java
+++ src/main/java/atm/Bank.java
@@ -1,20 +1,28 @@
 package atm;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 import java.util.Map;
 
 /**
  * A bank contains customers with bank accounts.
  */
+@Component
 public class Bank {
-
+   @Value("${bankname}")
    private String name;
    private Map<Integer,Customer> customers;
+   @Autowired
    private DataSource dataSource;
 
    /**
     * Constructs a bank with no customers.
     */
-   public Bank(String name, DataSource dataSource) {
+   @Autowired
+   public Bank(@Value("${bankname}") String name, DataSource dataSource) {
       this.name = name;
       this.dataSource = dataSource;
       this.customers = dataSource.readCustomers();
@@ -24,6 +32,10 @@ public class Bank {
     * Adds a customer to the bank.
     * @param customer the customer to add
     */
+   @PostConstruct
+   public void initCustomerData(){
+      this.customers = dataSource.readCustomers();
+   }
    public void registerCustomer(Customer customer) {
       customers.put(customer.getId(), customer);
    }
